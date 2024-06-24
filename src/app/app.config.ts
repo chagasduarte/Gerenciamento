64dae306d07659f8905dae951a9b2a8ebb3b4eee
Chagas Duarte <70854311+chagasduarte@users.noreply.github.com>
2024-06-24 19:01:37 +0000

    Adicionando services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
               provideRouter(routes), provideClientHydration(),
-              CommonModule
+              CommonModule,
+              provideHttpClient()
             ]
 };
