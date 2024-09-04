efa2d10f0e51e6e9a758b6091a56fd0c3f58955a
chagasduarte <chagasduarte.rdg@gmail.com>
2024-09-04 14:10:10 -0300

    implementando sistema de cores

--- src/app/pages/previstos/previstos.component.ts
+++ src/app/pages/previstos/previstos.component.ts
@@ -59,7 +59,7 @@ export class PrevistosComponent implements OnInit {
     this.router.navigate([""]);
   }
   DefineCorParcela(parcela: Parcela): string {
-    return parcela.isPaga? "#49865b": new Date(parcela.dataVencimento) < new Date()? "#af6e6e" : "#b1ca78";
+    return new Date(parcela.dataVencimento) < new Date()? "#af6e6e" : "#b1ca78";
   }
     
 }
