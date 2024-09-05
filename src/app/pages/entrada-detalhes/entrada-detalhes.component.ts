b6fbdbb7592e547acdd2f4dd01363dd26877905d
Chagas Duarte <70854311+chagasduarte@users.noreply.github.com>
2024-09-05 10:15:34 -0300

    Atualizar o entrada-detalhes.component.ts

--- src/app/pages/entrada-detalhes/entrada-detalhes.component.ts
+++ src/app/pages/entrada-detalhes/entrada-detalhes.component.ts
@@ -64,7 +64,7 @@ export class EntradaDetalhesComponent implements OnInit{
   }
 
   voltar() {
-    this.router.navigate([""]);
+    this.router.navigate(["home"]);
   }
   GetSalarioLiquido(salario: number): any {
     return GetSalarioLiquido(salario);
