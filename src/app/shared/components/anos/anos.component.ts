020fa4dbb967d256df308f1acadda019afe2dcd2
Chagas Duarte <francisco.c@luizalabs.com>
2026-01-03 17:36:01 -0300

    melhorias

--- src/app/shared/components/anos/anos.component.ts
+++ src/app/shared/components/anos/anos.component.ts
@@ -13,7 +13,7 @@ import { CommonModule } from '@angular/common';
 })
 export class AnosComponent {
   ano!: Ano;
-  anosDeDivida = [2024, 2025, 2026, 2027]
+  anosDeDivida = [ 2025, 2026, 2027 ]
   constructor(
     private readonly systemService: SystemService
   ){
