082f1f803d2dcb98bd8ec75a7cfa67ab09aa961d
Chagas Duarte <francisco.c@luizalabs.com>
2026-02-01 09:50:08 -0300

    ajustando rotas

--- src/app/pages/home/side-bar/side-bar.component.ts
+++ src/app/pages/home/side-bar/side-bar.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { SystemService } from '../../../shared/services/system.service';
 import { AuthService } from '../../../shared/services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { UsuarioService } from '../../../shared/services/usuario.service';
 import { Usuario } from '../../../shared/models/user.model';
 import { AnosComponent } from "../../../shared/components/anos/anos.component";
@@ -13,29 +13,30 @@ import { ResumoComponent } from "../../../shared/components/charts/resumo/resumo
   imports: [
     CommonModule,
     AnosComponent,
-    ResumoComponent
-],
+    ResumoComponent,
+    RouterModule
+  ],
   templateUrl: './side-bar.component.html',
   styleUrls: [
     './side-bar.component.css',
     './side-bar.component.mobile.css'
   ]
 })
-export class SideBarComponent implements OnInit{
+export class SideBarComponent implements OnInit {
   resumoMensal$ = this.systemService.resumo$; // <-- agora é reativo
   avatarUrl: string = "";
   usuario!: Usuario | null;
 
-  constructor(    
+  constructor(
     public readonly systemService: SystemService,
     private readonly authService: AuthService,
     private readonly router: Router,
     private readonly userService: UsuarioService
-  ){}
+  ) { }
   ngOnInit(): void {
     this.authService.usuario$.subscribe(user => this.usuario = user);
     this.userService.getAvatar(this.usuario!.id).subscribe({
-      next: (success) => {this.avatarUrl = success.avatarUrl;}
+      next: (success) => { this.avatarUrl = success.avatarUrl; }
     })
   }
   collapsed = false;
@@ -43,7 +44,7 @@ export class SideBarComponent implements OnInit{
   toggle() {
     this.collapsed = !this.collapsed;
   }
-  logout(){
+  logout() {
     this.authService.logout();
     this.router.navigate(["login"])
   }
